b121761ffc11c86209601843132b454a3da8610e
ericagomes-dev <ericagomesdev@gmail.com>
2026-06-06 10:06:40 -0300

    Use imdbId as key

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -4,7 +4,9 @@ import { MovieCard } from '../MovieCard/MovieCard';
 export const MovieList = ({ movies }) => (
   <div className="movie-list">
     {movies.map(movie => (
-      <MovieCard key={movie.id} movie={movie} />
+      <MovieCard
+       key={movie.imdbId}
+       movie={movie} />
     ))}
   </div>
 );
